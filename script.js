c3743b6e91518863cda2772af46f81b2a2cafef9
gchu79 <69404476+gchu79@users.noreply.github.com>
2020-08-11 22:43:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+class Bible {
+    constructor(outputBookTextElement, outputVersionTextElement, outputBookNumberElement) {
+        this.outputBookTextElement = outputBookTextElement
+        this.outputVersionTextElement = outputVersionTextElement
+        this.outputNumberTextElement = outputNumberTextElement
+    }    
+
+        clear() {
+        this.outputBookTextElement.innerHTML = ''
+        this.outputVersionTextElement.innerHTML = ''
+        this.outputNumberTextElement.innerHTML = ''        
+    }
+}
+
+const outputBookTextElement = document.querySelector('[output-book]')
+const outputVersionTextElement = document.querySelector('[output-version]')
+const outputNumberTextElement = document.querySelector('[output-number]')
+
+const bible = new Bible(outputBookTextElement, outputVersionTextElement, outputNumberTextElement)
+
+bible.clear()
+
+document.querySelectorAll('.bible-grid [data-book]').forEach(item => {
+    item.addEventListener('click', event => {
+        bible.outputBookTextElement.innerHTML = item.innerHTML
+    })
+})
+
+document.querySelectorAll('.version-grid [data-version]').forEach(item => {
+    item.addEventListener('click', event => {
+        bible.outputVersionTextElement.innerHTML = item.innerHTML      
+    })
+})
+
+document.querySelectorAll('.number-grid [data-number]').forEach(item => {
+    item.addEventListener('click', event => {
+        bible.outputNumberTextElement.innerHTML = item.innerHTML        
+    })
+})
+
+document.querySelectorAll('.version-grid [select-option]').forEach(item => {
+    item.addEventListener('click', event => {
+        if (bible.outputBookTextElement.innerHTML === '' || bible.outputVersionTextElement.innerHTML === '' || bible.outputNumberTextElement.innerHTML === '') return;
+        if (item.innerHTML === 'BibleHub') {
+            window.open("https://biblehub.com/" + bible.outputBookTextElement.innerHTML.replace(' ','_').toLowerCase() + "/" + bible.outputNumberTextElement.innerHTML + ".htm");        
+        }
+        if (item.innerHTML === 'BibleGateway') {
+            window.open("https://www.biblegateway.com/passage/?search=" + bible.outputBookTextElement.innerHTML.toLowerCase() + "%20" + bible.outputNumberTextElement.innerHTML + "&version=" + bible.outputVersionTextElement.innerHTML);        
+        }
+    })
+})
+
+document.querySelectorAll('.number-grid [clear-all]').forEach(item => {
+    item.addEventListener('click', event => {
+        bible.clear()      
+    })
+})
